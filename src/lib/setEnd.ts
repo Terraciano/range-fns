b78b8a876c3eeafca036ba5e0fb5e09868855168
Juan Lautaro Pereyra Terraciano <juan.pereyra@mediamonks.com>
2021-04-27 17:58:44 -0300

    remove comment and spreading in setEnd and setStart

--- src/lib/setEnd.ts
+++ src/lib/setEnd.ts
@@ -1,10 +1,7 @@
 import type { Range } from './types';
 
-// generic because we are keeping any extra properties from range
-
 export const setEnd = < T extends Range<number>>(range: T, end: number): T => {
   return {
-    ...range,
     start: Math.min(end, range.start),
     end,
   } as T;
--- src/lib/setStart.ts
+++ src/lib/setStart.ts
@@ -1,6 +1,5 @@
 import type { Range } from './types';
 
-// generic because we are keeping any extra properties from range
 export const setStart = < T extends Range<number>>(range: T, start: number): T => {
   return {
     start,
