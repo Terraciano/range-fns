87c1cffdf312ccfe1fda2572e90d8c783959ff3c
Juan Lautaro Pereyra Terraciano <juan.pereyra@mediamonks.com>
2021-04-28 10:53:28 -0300

    refactor clampRange.ts, add example comment

--- src/lib/clampRange.ts
+++ src/lib/clampRange.ts
@@ -1,7 +1,6 @@
 import { getDifference } from './getDifference';
 import type { Range } from './types';
 
-// might clamp to an invalid duration, so might return null
 export const clampRange = <T extends number>(
   range: Range<T>,
   rangeLimit: Range<T>,
@@ -11,6 +10,16 @@ export const clampRange = <T extends number>(
     end: Math.min(range.end, rangeLimit.end) as T,
   };
 
-  const duration = getDifference(newRange);
-  return duration > 0 ? newRange : null;
+  const difference = getDifference(newRange);
+  return difference > 0 ? newRange : null;
 };
+
+/*
+* Returns a clamped range between two ranges passed, if invalid, returns null
+*
+* const myRange = createRange(5, 10);
+* const limitRange = createRange(2, 7);
+*
+* console.log(clampRange(myRange,limitRange)) // {"start": 5,"end": 7}
+*
+* */
