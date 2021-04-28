4e7150eb9084401a84a7f1264fd245bb8ea9016f
Juan Lautaro Pereyra Terraciano <juan.pereyra@mediamonks.com>
2021-04-28 11:45:51 -0300

    add type to createRange example

--- src/lib/createRange.ts
+++ src/lib/createRange.ts
@@ -9,6 +9,6 @@ export const createRange = <T extends number>(start: T, end: T): Range<T> => {
  /*
  * Creates a range from the provided arguments
  *
- * console.log(createRange(5,10))  // {start: 5, end: 10}
+ * console.log(createRange<number>(5,10))  // {start: 5, end: 10}
  *
  * */
