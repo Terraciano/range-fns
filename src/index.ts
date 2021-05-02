9dfe281a816ade36ffa596d491d1a42b665dfdd6
terraciano <juan@wearesunrise.com>
2021-05-02 19:37:47 -0300

    add and organize exports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-export { addToRange } from './lib/addToRange';
-export { createRange } from './lib/createRange';
-export { clampRange } from './lib/clampRange';
-export { createRangeFromDuration } from './lib/createRangeFromDuration';
-export { getItemsInRange } from './lib/getItemsInRange';
-export { getLoopedRange } from './lib/getLoopedRange';
-export { getDifference } from './lib/getDifference';
-export { hasIntersection } from './lib/hasIntersection';
-export { moveRange } from './lib/moveRange';
-export { setEnd } from './lib/setEnd';
-export { setStart } from './lib/setStart';
-export { split } from './lib/split';
-export { splitRangeByDuration } from './lib/splitRangeByDuration';
-export { subtractFromRange } from './lib/subtractFromRange';
-export {toObject} from './lib/toObject'
-export {fromObject} from './lib/fromObject'
-export {getExclusion} from './lib/getExclusion'
-export type {Range,RangeObject} from './lib/types'
+export { addToRange } from './lib/addToRange'
+export { createRange } from './lib/createRange'
+export { clampRange } from './lib/clampRange'
+export { createRangeFromDuration } from './lib/createRangeFromDuration'
+export { fromObject } from './lib/fromObject'
+export { getItemsInRange } from './lib/getItemsInRange'
+export { getLoopedRange } from './lib/getLoopedRange'
+export { getDifference } from './lib/getDifference'
+export { getExclusion } from './lib/getExclusion'
+export { getCyclicRangeValues } from './lib/getCyclicRangeValues'
+export { getRangeValues } from './lib/getRangeValues'
+export { hasIntersection } from './lib/hasIntersection'
+export { moveRange } from './lib/moveRange'
+export { Range, RangeObject } from './lib/types'
+export { setEnd } from './lib/setEnd'
+export { setStart } from './lib/setStart'
+export { split } from './lib/split'
+export { splitRangeByDuration } from './lib/splitRangeByDuration'
+export { subtractFromRange } from './lib/subtractFromRange'
+export { toObject } from './lib/toObject'
