c62d870bec014bdaa60f45dfc79ace089718d9b0
terraciano <juan@wearesunrise.com>
2021-05-02 18:20:41 -0300

    refactor getExclusion.ts

--- src/lib/getExclusion.ts
+++ src/lib/getExclusion.ts
@@ -4,10 +4,5 @@ import { createRange } from './createRange'
 
 export const getExclusion = <T extends number>(rangeA: Range<T>, rangeB: Range<T>) => {
   if (!hasIntersection(rangeA, rangeB)) return []
-  return (
-    hasIntersection(rangeA, rangeB) && [
-      createRange(rangeA[0], rangeB[0]),
-      createRange(rangeA[1], rangeB[1]),
-    ]
-  )
+  return [createRange(rangeA[0], rangeB[0]), createRange(rangeA[1], rangeB[1])]
 }
